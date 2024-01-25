d4868e9b40302b0ebbf12a7b31992278b0f9e118
Ibrokhim1006 <Ibroxim.2001@mail.ru>
2024-01-25 15:22:32 -0500

    cors

--- core/settings.py
+++ core/settings.py
@@ -153,6 +153,7 @@ CORS_ALLOW_CREDENTIALS = True
 
 # cors
 CORS_ALLOWED_ORIGINS = [
+    "https://register-app.prounity.uz",
     "http://localhost:3000",
     "http://localhost:5174",
     "http://localhost:5173",
